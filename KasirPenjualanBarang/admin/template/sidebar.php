eb04b6694197711097736d2ce53bf8e5c6f57bfd
Zaldiarifm <93375769+Zaldiarifm@users.noreply.github.com>
2022-01-19 19:55:25 +0700

    Add files via upload

---
+++ KasirPenjualanBarang/admin/template/sidebar.php
@@ -0,0 +1,23 @@
+   <!-- **********************************************************************************************************************************************************
+      MAIN SIDEBAR MENU
+      *********************************************************************************************************************************************************** -->
+   <!--sidebar start-->
+   <aside>
+       <div id="sidebar" class="nav-collapse ">
+           <!-- sidebar menu start-->
+           <ul class="sidebar-menu" id="nav-accordion">
+
+               <p class="centered"><a><img src="assets/img/user/2557.jpg" class="img-circle" width="100" height="110"></a></p>
+               <h5 class="centered">Kelompok 4</h5>
+
+               <li class="mt">
+                   <a href="index.php">
+                       <i class="fa fa-dashboard"></i>
+                       <span>Halaman Utama</span>
+                   </a>
+               </li>
+           </ul>
+           <!-- sidebar menu end-->
+       </div>
+   </aside>
+   <!--sidebar end-->
